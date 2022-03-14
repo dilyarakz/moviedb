af201842ae74d4580496c579137d5f8b39fcb1b3
dida tulegenov <dida.tulegenov@fdmgroup.com>
2022-03-14 01:15:31 -0700

    removed svg icons

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -46,9 +46,6 @@ const Header = (props) => {
       <Box >
         <Link className="header-fav-box-link" to="/movie/favourites">
           <Text style={{ marginRight: "0.7rem" }}>Favourite </Text>
-          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fillRule="currentColor" className="bi bi-heart-fill" viewBox="0 0 16 16">
-            <path id="heartIcon" fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
-          </svg>
         </Link>
       </Box>
     </Flex>
